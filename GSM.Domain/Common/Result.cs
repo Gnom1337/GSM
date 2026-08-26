408185bdb706ec68ac6d7d40f15d3b84baa8ba20
Данил Грибов <danil.gribov.2002@mail.ru>
2026-08-26 22:38:39 +0300

    Add new result entity

---
+++ GSM.Domain/Common/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GSM.Domain.Common
+{
+    public class Result<TValue>
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public TValue? Value { get; }
+        public Error Error { get; }
+        private Result(TValue value) 
+        {
+            IsSuccess = true;
+            Value = value;
+            Error = Error.None;
+        }
+        public static Result<TValue> Success(TValue value) => new(value);
+        public static Result<Error> Failure(Error error) => new(error);
+    }
+    public record Error(string Code, string Description)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+    }
+}
